c18d9b5fb4d9435497adf7091466a09cb131fae7
Adam Kerz <github@kerz.id.au>
2016-01-22 15:25:52 +1000

    Adding decorator to do interactive request for commands

--- fabric_fixes/__init__.py
+++ fabric_fixes/__init__.py
@@ -1,3 +1,28 @@
 from .version import *
 # just importing does the work
 import monkeyPatch
+
+
+def acceptArgs(argList):
+    def decorator(fn):
+        @wraps(fn)
+        def taskFn(*args,**kwargs):
+            needed=[]
+            argsIndex=0
+            # figure out which arguments we've been provided with
+            for a in argList:
+                if a['name'] not in kwargs:
+                    if argsIndex>=len(args):
+                        needed.append(a)
+                    else:
+                        argsIndex+=1
+
+            # request the rest
+            for n in needed:
+                kwargs[n['name']]=raw_input('Enter a value for "{}" ({}): '.format(n['name'],n['type']))
+
+            return fn(*args,**kwargs)
+        return taskFn
+    return decorator
+
+# @acceptArgs([{'name':'environment','type':'string',},{'name':'scriptName','type':'string'}])
